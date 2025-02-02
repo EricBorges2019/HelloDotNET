bc7b3400cb3751346e7d945cf389f47f859f2ef8
ericborges2019 <9995803+EricBorges2019@users.noreply.github.com>
2025-02-02 11:23:10 -0500

    add file writing functionality to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Type something in for me to write to a file:");
+string input = Console.ReadLine();
+
+
+
+string path = "output.txt";
+
+File.WriteAllText(path, input);
+
+Console.WriteLine("In the file: " + path + " you wrote " + input + "!");
