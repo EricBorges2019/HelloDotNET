906694a114d61ceb57f736f257b62ea6c3977648
ericborges2019 <ericfborges@gmail.com>
2025-03-07 10:03:02 -0500

    Program now does multiple things

--- Program.cs
+++ Program.cs
@@ -3,13 +3,31 @@ using System.IO;
 
 
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Type something in for me to write to a file:");
-string input = Console.ReadLine();
+Console.WriteLine("Type in a key corresponding to something you want me to do:");
+Console.WriteLine("F to write something to a file;");
+Console.WriteLine("Q to quit.");
+
+string conInput = Console.ReadLine();
+//conInput is the console input at the start of the program
+
+if (conInput == "q" || conInput == "Q")
+// OR operator isn't recognized by compiler???
+{
+    Console.WriteLine("Quitting...");
+    return 0;
+}
+    if (conInput == "f" || conInput == "F")
+{
+    Console.WriteLine("Type something in for me to write to a file:");
+    string fileInput = Console.ReadLine();
 
 
 
-string path = "output.txt";
+    string path = "output.txt";
 
-File.WriteAllText(path, input);
+    File.WriteAllText(path, fileInput);
 
-Console.WriteLine("In the file: " + path + " you wrote " + input + "!");
+    Console.WriteLine("In the file: " + path + " you wrote " + fileInput + "!");
+    Console.WriteLine("Quitting...");
+}
+return 0;
